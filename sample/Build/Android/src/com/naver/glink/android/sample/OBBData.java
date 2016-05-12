0e5217dea1c9a2bae4cbb883df8ad031cdc98a42
김원겸 <naver@naverui-MacBook-Pro-6.local>
2016-05-12 10:38:25 +0900

    obb 관련 파일 수정.

--- sample/Build/Android/src/com/naver/glink/android/sample/OBBData.java
+++ sample/Build/Android/src/com/naver/glink/android/sample/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-0L // the length of the file in bytes
+73497382L // the length of the file in bytes
 )
 };
 };
