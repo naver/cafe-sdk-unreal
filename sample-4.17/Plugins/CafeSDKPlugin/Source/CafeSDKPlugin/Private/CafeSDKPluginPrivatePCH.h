21411d4b303cd62b35b5f804aad711f2d47d582c
Sangin <sangin.kang@navercorp.com>
2019-02-14 17:02:38 +0900

    Delete Unreal 4.17 sample

--- sample-4.17/Plugins/CafeSDKPlugin/Source/CafeSDKPlugin/Private/CafeSDKPluginPrivatePCH.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2016 NAVER Corp. All rights reserved.
-
-#include "CafeSDKPlugin.h"
-
-// You should place include statements to your module's private header files here.  You only need to
-// add includes for headers that are used in most of your module's source files though.
